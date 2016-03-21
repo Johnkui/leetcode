b6db463ca997e47af72b2fdeb59b17c83a6340e6
wenzengkui <wenzengkui@bytedance.com>
2016-03-21 22:34:34 +0800

    search in rotated sort array

--- LeetCode/LeetCode/main.mm
+++ LeetCode/LeetCode/main.mm
@@ -7,13 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <time.h>
 #import "33_Search_in_Rotated_Sorted_Array_h.hpp"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        vector<int> vec
+
+        vector<int> nums;
+        nums.push_back(1);
+        nums.push_back(3);
+        nums.push_back(5);
+        
+        int target = 4;
+        Solution s;
+        
+        clock_t start, end;
+        start = clock();
+        int index = s.search(nums, target);
+        end = clock();
+        
+        NSLog(@"index = %d, time = %lf", index, (end - start) * 1000.0 / (CLOCKS_PER_SEC));
+        
     }
     return 0;
 }
