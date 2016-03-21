e27bda184c03d54591377541a82792fac8425a81
wenzengkui <wenzengkui@bytedance.com>
2016-03-21 22:00:20 +0800

    Search in rotated array

---
+++ LeetCode/LeetCode/33_Search_in_Rotated_Sorted_Array_h.hpp
@@ -0,0 +1,14 @@
+//
+//  33_Search_in_Rotated_Sorted_Array_h.hpp
+//  LeetCode
+//
+//  Created by Johnkui Wen on 16/3/21.
+//  Copyright © 2016年 Johnkui Wen. All rights reserved.
+//
+
+#ifndef _3_Search_in_Rotated_Sorted_Array_h_hpp
+#define _3_Search_in_Rotated_Sorted_Array_h_hpp
+
+#include <stdio.h>
+
+#endif /* _3_Search_in_Rotated_Sorted_Array_h_hpp */
---
+++ LeetCode/LeetCode/main.mm
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by Johnkui Wen on 16/3/21.
+//  Copyright © 2016年 Johnkui Wen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "33_Search_in_Rotated_Sorted_Array_h.hpp"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        vector<int> vec
+    }
+    return 0;
+}
